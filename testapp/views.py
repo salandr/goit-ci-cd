2ae7a51322743735ad2473071192574192aa99f1
salandr <ansaliy200013@gmail.com>
2025-09-11 23:24:57 +0100

    lesson-5

--- testapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from .models import Message
-
-def show_message(request):
-    msg = Message.objects.first()
-    if msg:
-        return HttpResponse(f"Mesage from db: {msg.text}")
-    return HttpResponse("No messages")
